67ffc2bf4c3b1e1140abdca3d8428165fa365767
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-10-13 10:55:54 -0700

    Added user signup tests.

--- test_user_model.py
+++ test_user_model.py
@@ -7,8 +7,16 @@
 
 import os
 from unittest import TestCase
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import UniqueViolation
 
-from models import db, User, Message, Follow
+from models import (db,
+                    User,
+                    Message,
+                    Follow,
+                    DEFAULT_HEADER_IMAGE_URL,
+                    DEFAULT_IMAGE_URL)
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -28,6 +36,8 @@ from app import app
 db.drop_all()
 db.create_all()
 
+bcrypt = Bcrypt()
+
 
 class UserModelTestCase(TestCase):
     """Tests for User model."""
@@ -45,11 +55,13 @@ class UserModelTestCase(TestCase):
         self.u2_id = u2.id
 
     def tearDown(self):
-        """Reset"""
-        
+        """Rollback fouled transactions"""
+
         db.session.rollback()
 
     def test_user_model(self):
+        """Test User model relationships for newly created users."""
+
         u1 = User.query.get(self.u1_id)
         u2 = User.query.get(self.u2_id)
 
@@ -63,13 +75,90 @@ class UserModelTestCase(TestCase):
         self.assertEqual(len(u2.liked_messages),0)
 
     def test_is_following_and_followed_by(self):
+        """Test is_following and followed_by instance methods."""
+
         u1 = User.query.get(self.u1_id)
         u2 = User.query.get(self.u2_id)
 
-        # test is following
         self.assertFalse(u1.is_following(u2))
+        self.assertFalse(u2.is_followed_by(u1))
 
-        u1.followers.append(u2)
+        # Have u1 follow u2.
+        u2.followers.append(u1)
         db.session.commit()
 
         self.assertTrue(u1.is_following(u2))
+        self.assertTrue(u2.is_followed_by(u1))
+
+    def test_user_signup_success(self):
+        """Test signing up a new user."""
+
+        username = "u3"
+        email = "u3@email.com"
+        password = "password"
+
+        u3 = User.signup(username, email, password, None)
+
+        db.session.commit()
+
+        u3 = User.query.filter(User.username==username).one_or_none()
+
+        self.assertTrue(u3.username == username)
+        self.assertTrue(u3.email == email)
+        self.assertTrue(
+            bcrypt.check_password_hash(u3.password, password)
+            )
+        self.assertTrue(u3.image_url == DEFAULT_IMAGE_URL)
+        self.assertTrue(u3.header_image_url == DEFAULT_HEADER_IMAGE_URL)
+        self.assertTrue(u3.bio == "")
+        self.assertTrue(u3.location == "")
+
+    def test_user_signup_fail(self):
+        """Test signing up a new user with invalid credentials."""
+
+        invalid_username = "u2" # Username taken
+        invalid_email = "u2@email.com" # Email taken
+
+        valid_username = "u3"
+        valid_email = "u3@email.com"
+        password = "password"
+
+        # Invalid username
+        user = User.signup(invalid_username, valid_email, password, None)
+
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+        db.session.rollback()
+
+        # Invalid email
+        user = User.signup(valid_username, invalid_email, password, None)
+
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+        db.session.rollback()
+
+        # Empty username
+        user = User.signup(None, valid_email, password, None)
+
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+        db.session.rollback()
+
+        # Empty email
+        user = User.signup(valid_username, None, password, None)
+
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+        db.session.rollback()
+
+        # Empty password
+        with self.assertRaises(ValueError):
+            user = User.signup(valid_username, valid_email, None, None)
+
+
+
+
