667fc91c3a6dad0040907eec04f42b5143723417
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-12-28 09:14:49 -0800

    Add like view tests.

--- test_user_views.py
+++ test_user_views.py
@@ -25,7 +25,7 @@ db.create_all()
 bcrypt = Bcrypt()
 
 
-class UserRoutesTestCase(TestCase):
+class UserViewsTestCase(TestCase):
     """Tests for User routes."""
 
     def setUp(self):
@@ -44,120 +44,6 @@ class UserRoutesTestCase(TestCase):
         """Clean up fouled transactions."""
 
         db.session.rollback()
-
-    def test_get_signup(self):
-        """ Test user signup GET request."""
-
-        with app.test_client() as client:
-            resp = client.get("/signup")
-            html = resp.get_data(as_text=True)
-
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("TEST: signup.html", html)
-
-
-    def test_post_signup_ok(self):
-        """Test a valid signup."""
-
-        with app.test_client() as client:
-            resp = client.post(
-                "/signup",
-                data={
-                    "username": "u3",
-                    "password": "password",
-                    "email": "u3@email.com"
-                },
-                follow_redirects=True,
-            )
-
-            self.assertEqual(resp.status_code, 200)
-
-            u = User.query.filter(User.username == "u3").one_or_none()
-
-            self.assertEqual(u.email, "u3@email.com")
-
-    def test_register_username_taken(self):
-        """Test a signup with existing username."""
-
-        with app.test_client() as client:
-            resp = client.post(
-                "/signup",
-                data={
-                    "username": "u1",
-                    "password": "password",
-                    "email": "u3@email.com"
-                }
-            )
-            html = resp.get_data(as_text=True)
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("Username or email already taken", html)
-
-    def test_register_email_taken(self):
-        """Test a signup with existing email."""
-
-        with app.test_client() as client:
-            resp = client.post(
-                "/signup",
-                data={
-                    "username": "u3",
-                    "password": "password",
-                    "email": "u1@email.com"
-                }
-            )
-            html = resp.get_data(as_text=True)
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("Username or email already taken", html)
-
-    def test_post_login_ok(self):
-        """
-        Test valid login POST.
-        """
-
-        with app.test_client() as client:
-            resp = client.post(
-                "/login",
-                data={
-                    "username": "u1",
-                    "password": "password",
-                },
-                follow_redirects=True,
-            )
-            html = resp.get_data(as_text=True)
-
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("TEST: home.html", html)
-            self.assertEqual(session.get(CURR_USER_KEY), self.u1_id)
-
-    def test_post_login_bad(self):
-        """
-        Test bad login POST.
-        """
-
-        with app.test_client() as client:
-            resp = client.post(
-                "/login",
-                data={
-                    "username": "u1",
-                    "password": "foofoo",
-                },
-                follow_redirects=True
-            )
-
-            html = resp.get_data(as_text=True)
-
-            self.assertEqual(resp.status_code, 200)
-            self.assertEqual(session.get(CURR_USER_KEY), None)
-            self.assertIn("Invalid credentials.", html)
-
-    def test_login_form(self):
-        """Test getting the login form."""
-
-        with app.test_client() as client:
-            resp = client.get("/login")
-            html = resp.get_data(as_text=True)
-            
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("TEST: login.html", html)
 
     def test_login_following_page(self):
         """
@@ -309,28 +195,21 @@ class UserRoutesTestCase(TestCase):
             self.assertIn("Access unauthorized", html)
 
 
-class LikeRoutesTestCase(TestCase):
-    """Tests for routes related to likes."""
-
+class UserLikeTestCase(UserViewsTestCase):
     def setUp(self):
-        """Make demo data."""
-
-        User.query.delete()
-        Message.query.delete()
-        Like.query.delete()
-
-        u1 = User.signup("u1", "u1@email.com", "password", None)
-        u2 = User.signup("u2", "u2@email.com", "password", None)
-
+        super().setUp()
+        u3 = User.signup("u3", "u3@email.com", "password", None)
+        db.session.add_all([u3])
+        db.session.flush()
+        m1 = Message(text="text", user_id=self.u2_id)
+        db.session.add_all([m1])
+        db.session.flush()
+        k1 = Like(user_id=self.u2_id, message_id=m1.id)
+        db.session.add_all([k1])
         db.session.commit()
 
-        self.u1_id = u1.id
-        self.u2_id = u2.id
-
-        Message(id=1, text="test", user_id=self.u1_id)
-        Like(user_id=self.u2_id, message_id=1)
-
-        db.session.commit()
+        self.u3_id = u3.id
+        self.m1_id = m1.id
 
     def tearDown(self):
         """Clean up fouled transactions."""
@@ -368,4 +247,53 @@ class LikeRoutesTestCase(TestCase):
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Access unauthorized", html)
+
+    def test_add_like(self):
+        """
+        Test liking a message.
+        """
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u3_id
+
+            resp = client.post(
+                f"/messages/{self.m1_id}/like",
+                follow_redirects=True,
+                data={
+                    "requesting_url": f"/users/{self.u3_id}/likes"
+                }
+            )
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("TEST: user show_likes.html", html)
+            
+            likes = Like.query.filter(Like.message_id == self.m1_id, ).all()
+            self.assertEqual(len(likes), 2)
+
+    def test_remove_like(self):
+        """
+        Test removing a like from a message.
+        """
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u2_id
+
+            resp = client.post(
+                f"/messages/{self.m1_id}/like/delete",
+                follow_redirects=True,
+                data={
+                    "requesting_url": f"/users/{self.u3_id}/likes"
+                }
+            )
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("TEST: user show_likes.html", html)
+            
+            likes = Like.query.filter(Like.message_id == self.m1_id, ).all()
+            self.assertEqual(len(likes), 0)
+
 
